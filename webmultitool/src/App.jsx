b7a8a936f338883270b9b62c90769b9befc65cea
lilexc3 <nemokidmooker@gmail.com>
2026-04-27 17:14:14 +0200

    Refresh old css styles

--- webmultitool/src/App.jsx
+++ webmultitool/src/App.jsx
@@ -1,10 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./app.css";
 import StartBanner from "./components/app/start-banner";
 import Step1 from "./components/app/step1";
 import Step2 from "./components/app/step2";
 import LogInPage from "./components/logInAndSignUp/log-in-page";
 import SignUpPage from "./components/logInAndSignUp/sign-up-page";
+import DashboardLayout from "./components/dashboard/dashboard-layout";
+import SitesList from "./components/dashboard/sites-list";
+import SiteDetail from "./components/dashboard/site-detail";
+import SiteNew from "./components/dashboard/site-new";
+import DashboardSettings from "./components/dashboard/dashboard-settings";
 
 function App() {
   return (
@@ -23,8 +28,17 @@ function App() {
             </div>
           }
         />
+
         <Route path="/log-in" element={<LogInPage />} />
         <Route path="/sign-up" element={<SignUpPage />} />
+
+        <Route path="/dashboard" element={<DashboardLayout />}>
+          <Route index element={<Navigate to="/dashboard/sites" replace />} />
+          <Route path="sites" element={<SitesList />} />
+          <Route path="sites/new" element={<SiteNew />} />
+          <Route path="sites/:id" element={<SiteDetail />} />
+          <Route path="settings" element={<DashboardSettings />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
